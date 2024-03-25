e5a38969a81e99b8d9b7d86944b6082dd8ac7014
WillRob <william.roberge.wr@gmail.com>
2024-03-25 12:53:33 -0400

    add player class

--- Template/src/main.py
+++ Template/src/main.py
@@ -1,4 +1,8 @@
+import random
+import string
+
 from data.data import data
+from player.player import player
 
 
 # Définition de la fonction main
@@ -6,7 +10,9 @@ def main():
     print("MAIN")
     path = r"C:\Users\willi\Documents\Perso\cyber-infra\docker-infra\Template\data.xlsx"
     datastruct = data(path)
-    docker_compose_content = generer_docker_compose(datastruct)
+
+    equipe_bleu, equipe_rouge = generer_equipe(datastruct)
+    docker_compose_content = generer_docker_compose(equipe_bleu, equipe_rouge)
 
     # Écrire le contenu dans un fichier docker-compose.yml
     with open(r"C:\Users\willi\Documents\Perso\cyber-infra\docker-infra\Template\dockerCompose\docker-compose.yml", 'w') as file:
@@ -14,42 +20,64 @@ def main():
 
     print("Le fichier docker-compose.yml a été généré avec succès.")
 
-
-def generer_docker_compose(datastruct):
-    docker_compose = "version: '3.7'\nservices:\n"
-    proxy_count = 1
-    ssh_pass = "root"
+def generer_equipe(datastruct):
+    equipe_bleu = []
+    equipe_rouge = []
     port_count = 2222  # Commence à exposer les conteneurs à partir de ce port
 
-     # Générer les services Ubuntu pour l'équipe Bleue
-    for i in range(1, datastruct.get_nb_bleu() + 1):
+    # Création des joueurs de l'équipe Bleue
+    for i in range(datastruct.get_nb_bleu()):
+        prenom, nom = datastruct.getParticipantBleu(i)
+        mots = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+        joueur = player(prenom, nom, "", "bleu", port_count, mots)
+        equipe_bleu.append(joueur)
+        port_count += 1
+
+    # Création des joueurs de l'équipe Rouge
+    for i in range(datastruct.get_nb_rouge()):
+        prenom, nom = datastruct.getParticipantRouge(i)
+        mots = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+        joueur = player(prenom, nom, "", "rouge", port_count, mots)
+        equipe_rouge.append(joueur)
+        port_count += 1
+
+    return equipe_bleu, equipe_rouge
+
+def generer_docker_compose(equipe_bleu, equipe_rouge):
+    docker_compose = "version: '3.7'\nservices:\n"
+    ssh_pass = "root"
+    compteur_bleu = 1
+    compteur_rouge = 1
+
+    # Générer les services Ubuntu pour l'équipe Bleue
+    for joueur in equipe_bleu:
         docker_compose += (
-            f"  ubuntubleu{i}:\n"
+            f"  ubuntubleu{compteur_bleu}:\n"
             f"    build:\n"
             f"      context: ../Ubuntu\n"
             f"      args:\n"
-            f"        SSH_PASS: {ssh_pass}\n"
-            f"    container_name: ubuntu_ssh_Bleu{i}\n"
+            f"        SSH_PASS: {joueur.motspass}\n"
+            f"    container_name: ubuntu_bleu_{compteur_bleu}\n"
             f"    ports:\n"
-            f"      - '{port_count}:{port_count}'\n"
+            f"      - '{joueur.port}:22'\n"
         )
-        port_count += 1  # Incrémente le numéro de port pour le prochain conteneur
+        compteur_bleu += 1
 
     # Générer les services Ubuntu pour l'équipe Rouge
-    for i in range(1, datastruct.get_nb_rouge() + 1):
+    for joueur in equipe_rouge:
         docker_compose += (
-            f"  ubunturouge{i}:\n"
+            f"  ubunturouge{compteur_rouge}:\n"
             f"    build:\n"
             f"      context: ../Ubuntu\n"
             f"      args:\n"
-            f"        SSH_PASS: {ssh_pass}\n"
-            f"    container_name: ubuntu_ssh_Rouge{i}\n"
+            f"        SSH_PASS: {joueur.motspass}\n"
+            f"    container_name: ubuntu_rouge_{compteur_rouge}\n"
             f"    ports:\n"
-            f"      - '{port_count}:{port_count}'\n"
+            f"      - '{joueur.port}:22'\n"
         )
-        port_count += 1  # Continue d'incrémenter le numéro de port pour chaque conteneur
+        compteur_rouge += 1
 
-        # Déclaration du réseau personnalisé "hack réseau"
+    # Déclaration du réseau personnalisé "hack réseau"
     docker_compose += (
         "\nnetworks:\n"
         "  reseau:\n"
@@ -57,6 +85,8 @@ def generer_docker_compose(datastruct):
     )
 
     return docker_compose
+
+
 
 # Vérification si le script est exécuté directement
 if __name__ == "__main__":
---
+++ Template/src/player/player.py
@@ -0,0 +1,10 @@
+class player:
+   def __init__(self, prenom, nom, mail, team, port, motspass):
+        self.prenom = prenom
+        self.nom = nom
+        self.mail = mail
+        self.team = team
+        self.port = port
+        self.motspass = motspass
+
+    
